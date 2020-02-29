b4ff2c064af165dc01cfe1174e497658b3fc6fad
jakuberski <60200680+jakuberski@users.noreply.github.com>
2020-02-29 10:27:36 -0600

    Test push
    Test push

--- Animal Database/src/zooTester.java
+++ Animal Database/src/zooTester.java
@@ -14,31 +14,26 @@ public class zooTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Animals animal1 = new Animals("lion");
-		
+
 		InsertAnimal ah = new InsertAnimal();
-		
+
 		ah.insertAnimal(animal1);
-		
+
 		AttractionsHelper ahh = new AttractionsHelper();
-		
+
 		Attractions animal1List = new Attractions("Animal 1 List", LocalDate.now(), animal1);
-		
+
 		ahh.insertNewAttractions(animal1List);
-		
+
 		List<Attractions> allAttractions = ahh.getAttractions();
-		
-		for(Attractions a : allAttractions) {
+
+		for (Attractions a : allAttractions) {
 			System.out.println(a.toString());
+			
+			//this test 
 		}
-		
-		
-		
-	
-		
-		
-		
 
 	}
 
