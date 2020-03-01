695b2bcc2ec99a3f19f27699d8b827f9f08cac78
jakuberski <60200680+jakuberski@users.noreply.github.com>
2020-03-01 16:20:37 -0600

    created Zoo Servlet
    and zoo-list.jsp

--- Animal Database/src/model/Attractions.java
+++ Animal Database/src/model/Attractions.java
@@ -1,39 +1,58 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
 @Entity
-@Table(name="attractions")
+@Table(name = "attractions")
 public class Attractions {
-@Id
-@GeneratedValue(strategy=GenerationType.IDENTITY)
-@Column(name="ATTRACTION_ID")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ATTRACTION_ID")
 	private int id;
-@Column(name="LOCATION")
+	@Column(name = "LOCATION")
 	private String location;
-@Column(name="TRIP_DATE")
+	@Column(name = "TRIP_DATE")
 	private LocalDate tripDate;
-@ManyToOne
+	@ManyToOne
 	private Animals animal;
+	@OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+	@JoinTable(name = "ITEMS_ON_LIST", joinColumns = {
+			@JoinColumn(name = "LIST_ID", referencedColumnName = "LIST_ID") }, inverseJoinColumns = {
+					@JoinColumn(name = "ITEM_ID", referencedColumnName = "ID", unique = true) })
+	private List<Animals> listOfAttractions;
 
 	public Attractions() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Attractions(int id, String location, LocalDate tripDate, Animals animal) {
+	public Attractions(int id, String location, LocalDate tripDate, Animals animal, List<Animals> listOfAttractions) {
 		super();
 		this.id = id;
 		this.location = location;
 		this.tripDate = tripDate;
 		this.animal = animal;
+		this.listOfAttractions = listOfAttractions;
+	}
+
+	public Attractions(String location, LocalDate tripDate, Animals animal, List<Animals> listOfAttractions) {
+		super();
+		this.location = location;
+		this.tripDate = tripDate;
+		this.animal = animal;
+		this.listOfAttractions = listOfAttractions;
 	}
 
 	public Attractions(String location, LocalDate tripDate, Animals animal) {
@@ -41,9 +60,16 @@ public class Attractions {
 		this.location = location;
 		this.tripDate = tripDate;
 		this.animal = animal;
+
 	}
-	
-	
+
+	public List<Animals> getListOfAttractions() {
+		return listOfAttractions;
+	}
+
+	public void setListOfAttractions(List<Animals> listOfAttractions) {
+		this.listOfAttractions = listOfAttractions;
+	}
 
 	public int getId() {
 		return id;
@@ -80,7 +106,7 @@ public class Attractions {
 	@Override
 	public String toString() {
 		return "Attractions [id=" + id + ", location=" + location + ", tripDate=" + tripDate + ", animal=" + animal
-				+ "]";
+				+ ", listOfAttractions=" + listOfAttractions + "]";
 	}
 
 }
